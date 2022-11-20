f27b3ebd323165f83275f52775927c9476b224c5
PawelMiskowiec <pawelmiskowiec1@gmail.com>
2022-11-20 11:17:38 +0100

    Lab2 Adapter

---
+++ src/main/java/pl/edu/wszib/lab2/decorator/Application.java
@@ -0,0 +1,16 @@
+package pl.edu.wszib.lab2.decorator;
+
+public class Application {
+
+    public static void main(String[] args) {
+        OrderService simpleOrderService = new SimpleOrderService();
+        Integer testName = simpleOrderService.handle("Test name");
+
+        OrderService decoratedOrderService = new MultiplyWhenContainTestOrderServiceDecorator(
+                new LogAroundOrderServiceDecorator(simpleOrderService)
+        );
+        decoratedOrderService.handle("Test name");
+
+        // TODO zad: utworzyć Decorator, który dla nazw zawierających słówko "test" podwaja zwracaną wartość
+    }
+}
